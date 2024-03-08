14454ef599fbd9f7d9739eed84ceb07ab723ba37
hommehyuk <bin6827@gmail.com>
2024-03-08 17:28:26 +0900

    PageController 사용

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,21 +12,36 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Timer? timer;
+  PageController controller = PageController(
+    initialPage: 0,
+  );
 
   @override
   void initState() {
     super.initState();
 
     timer = Timer.periodic(
-      Duration(seconds: 1),
+      const Duration(seconds: 4),
       (timer) {
-        print('Timer!');
+        int currentPage = controller.page!.toInt();
+        int nextPage = currentPage + 1;
+
+        if (nextPage > 4) {
+          nextPage = 0;
+        }
+
+        controller.animateToPage(
+          nextPage,
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.linear,
+        );
       },
     );
   }
 
   @override
   void dispose() {
+    controller.dispose();
     if (timer != null) {
       timer!.cancel();
     }
@@ -34,8 +50,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: [1, 2, 3, 4, 5]
             .map(
               (e) => Image.asset(
